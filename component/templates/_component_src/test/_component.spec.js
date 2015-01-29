f068a8b3dd6cd91ce47e5ad76e8b15e145850500
Edward Knowles <mail@eknowles.com>
2015-01-29 21:23:54 +0000

    feat(tests): add example test

--- component/templates/_component_src/test/_component.spec.js
+++ component/templates/_component_src/test/_component.spec.js
@@ -1,5 +1,22 @@
 describe('<%= componentModuleName %>', function () {
-  beforeEach(module('<%= componentModuleName %>', '<%= componentTplDir %>/<%= componentId %>.html'));
+
+  var $scope;
+
+  beforeEach(module('<%= componentModuleName %>'));
+  beforeEach(module('<%= componentTplDir %>/<%= componentId %>.html'));
+
+  beforeEach(inject(function ($rootScope) {
+    $scope = $rootScope;
+  }));
+
+  // http://jasmine.github.io/2.0/introduction.html
+
+  describe("this thing", function () {
+    it("should be true", function () {
+      expect(true).toBe(true);
+    });
+
+  });
 
 
 });
